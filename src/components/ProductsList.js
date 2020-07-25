46cfbc32ea2917d2a0c1e375106e49549fef065a
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-07-25 18:40:22 +0900

    Add css styling to product list page

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -4,294 +4,264 @@ import styled from 'styled-components';
 const ProductsList = () => {
   return (
     <section>
-      <div>
+      <ProductsContainer>
         <CategoryTitle>タイムセール中の人気商品</CategoryTitle>
         <Container>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1262&q=80"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1516726817505-f5ed825624d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1539655382699-69e1d1979ee0?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1592301933927-35b597393c0a?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1576062086176-85d700ae1695?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
-          </Card>
-          <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
-          </Card>
-          <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
-          </Card>
-          <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1485462537746-965f33f7f6a7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
         </Container>
+      </ProductsContainer>
+      <ProductsContainer>
         <CategoryTitle>タイムセール中の人気商品</CategoryTitle>
         <Container>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1262&q=80"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1516726817505-f5ed825624d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1539655382699-69e1d1979ee0?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1592301933927-35b597393c0a?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1576062086176-85d700ae1695?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
-          </Card>
-          <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
-          </Card>
-          <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
-          </Card>
-          <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1485462537746-965f33f7f6a7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
         </Container>
-      </div>
-      <div>
+      </ProductsContainer>
+      <ProductsContainer>
         <CategoryTitle>タイムセール中の人気商品</CategoryTitle>
         <Container>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1262&q=80"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1516726817505-f5ed825624d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1539655382699-69e1d1979ee0?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1592301933927-35b597393c0a?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1576062086176-85d700ae1695?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
           <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
-          </Card>
-          <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
-          </Card>
-          <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
-          </Card>
-          <Card>
-            {/* <CardImg src="" alt="" /> */}
-            {/* <CardOptions> */}
-            <Name>unknow brand</Name>
-            {/* <InnerContainer>
-              <Price>¥4,290</Price>
-              <Heart>Heart</Heart> */}
-            {/* </InnerContainer> */}
-            {/* </CardOptions> */}
+            <CardImg
+              src="https://images.unsplash.com/photo-1485462537746-965f33f7f6a7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80"
+              alt=""
+            />
+            <CardOptions>
+              <Name>unknow brand</Name>
+              <InnerContainer>
+                <Price>¥4,290</Price>
+                <Heart>H</Heart>
+              </InnerContainer>
+            </CardOptions>
           </Card>
         </Container>
-      </div>
+      </ProductsContainer>
     </section>
   );
 };
 
 export default ProductsList;
+
+const ProductsContainer = styled.div`
+  margin-bottom: 24px;
+`;
 
 const CategoryTitle = styled.h3`
   font-size: 16px;
@@ -306,7 +276,6 @@ const Container = styled.div`
   flex-direction: row;
   justify-content: space-between;
   flex-wrap: wrap;
-  margin-bottom: 24px;
 `;
 
 const Card = styled.div`
@@ -318,10 +287,34 @@ const Card = styled.div`
 `;
 
 const Name = styled.p`
-  font-size: 16px;
+  font-size: 14px;
+  line-height: 1;
+  margin-bottom: 8px;
+  font-family: 'Roboto', sans-serif;
 `;
 
 const Price = styled.span`
   font-size: 20px;
   color: #333333;
+  line-height: 1;
+  font-family: 'Roboto', sans-serif;
 `;
+
+const CardImg = styled.img`
+  width: 100%;
+  max-height: 144px;
+  /* Trimming */
+  object-fit: cover;
+`;
+
+const CardOptions = styled.div`
+  padding: 8px;
+`;
+const InnerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+/* Temporary */
+const Heart = styled.span``;
